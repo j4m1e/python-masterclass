9bcac59457051acf072c99b20f0e001364501029
j4m1e <jamie@thetylers.me.uk>
2020-01-13 14:01:14 +0000

    added class inheritance

--- classes-n-objects.py
+++ classes-n-objects.py
@@ -6,7 +6,7 @@ attribute values for a particular object define the state of that object. Anothe
 inheritance, this means that the new class may inherit all the names and functionality from an existing class  
 """
 # define a class. The convention is to use camel case in the class name. If the class doesnt inherit from another class
-# then the word object should be used within the perentheses 
+# then the word object (inherits from the default object class) should be used within the perentheses 
 class MyRouter(object):
     # doc string like functions can be used here
     "This is a class that describes the characteristics of a router."
@@ -83,3 +83,42 @@ AttributeError: 'MyRouter' object has no attribute 'ios'
 """
 # verify that an object is an instance of a particular class
 print(f"Check that router2 is an instance of the MyRouter class: {isinstance(router2, MyRouter)}")
+"""
+Class Inheritance. Have one class that inherits attributes and methods from an existing class. The class inheriting
+the attributes and methods is a child class and the class that the child inherits from is called a parent. To inherit
+from a certain class the name of the class is placed between the parenthensis of the child class definition instead of
+object. 
+
+A child class can inherit from 2 or more parents at the same time. This is achieved by the following.
+'class ChildClass(ParentClass1, ParentClass2, ParentClass3):'
+"""
+# define the new class. The init method for a child class can be omitted as the child extends the parent class and
+# therefore inherited from the parent. If there are new attributes to define as well as inheriting then the init
+# method needs to be redefined but telling Python to inherit the attributes and also add the new ones
+print(f"CLASS INHERITANCE")
+class MyNewRouter(MyRouter):
+    # inserted all the attributes from the parent class plus the new attribute for the child (portsno)
+    def __init__(self, routername, model, serialno, ios, portsno):
+        # call the init method from the parent class. which is like importing them from the parent class
+        MyRouter.__init__(self, routername, model, serialno, ios)
+        # define the new attribute specific to this class only called portsno 
+        self.portsno = portsno 
+    # define a method inside the child class that concatinates a string argument when calling the method & the value
+    # of the model attribute inherited from the parent
+    def printNewRouter(self, string):
+        print(f"{string}{self.model}")
+
+print(f"Creating an object from the class MyNewRouter called newRouter1")
+newRouter1 = MyNewRouter("newr1", "1800", "11111", "12.2", "10")
+print(f"{newRouter1}")
+print(f"The portsno attribute for the object newRouter1 is {newRouter1.portsno}")
+# access the attributes from the parent class using the instance of the child
+print(f"The parent class attribute model is {newRouter1.model}")
+print(f"The parent class attribute ios is {newRouter1.ios}")
+# access a method from the parent class
+print(f"Calling the parent class method printRouter()")
+newRouter1.printRouter("20200101")
+print(f"Calling the child class method printNewRouter()")
+newRouter1.printNewRouter("20200101")
+# check if a class is a child of another 
+print(f"Is the class MyNewRouter a child of MyRouter: {issubclass(MyNewRouter, MyRouter)}")
